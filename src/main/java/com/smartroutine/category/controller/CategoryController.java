f7a98eda59d68f9f42316301bb83f92c77ee3ef1
sohyun119 <sh-119@naver.com>
2026-03-13 00:32:22 +0900

    ✨ feat: 카테고리 목록 조회 Controller

--- src/main/java/com/smartroutine/category/controller/CategoryController.java
+++ src/main/java/com/smartroutine/category/controller/CategoryController.java
@@ -2,11 +2,14 @@ package com.smartroutine.category.controller;
 
 import com.smartroutine.category.dto.CategoryCreateRequest;
 import com.smartroutine.category.dto.CategoryCreateResponse;
+import com.smartroutine.category.dto.CategoryReadResponse;
 import com.smartroutine.category.service.CategoryService;
+import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,14 +21,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoryController {
 
     private final CategoryService categoryService;
+    private static final UUID TEST_USER_ID = UUID.randomUUID();
 
     @PostMapping
     public ResponseEntity<CategoryCreateResponse> create(@RequestBody CategoryCreateRequest request) {
-        // 임시 userId
-        UUID userId = UUID.randomUUID();
-        CategoryCreateResponse response = categoryService.createCategory(userId, request);
+
+        CategoryCreateResponse response = categoryService.createCategory(TEST_USER_ID, request);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CategoryReadResponse>> readCategories() {
+
+        List<CategoryReadResponse> responses = categoryService.readCategories(TEST_USER_ID);
+
+        return ResponseEntity.ok(responses);
     }
 
 
