90bc5be6445dc23492177df6bd913f6414585b4d
sohyun119 <sh-119@naver.com>
2026-03-13 00:32:55 +0900

    ✨ feat: 카테고리 목록 조회 Repository

--- src/main/java/com/smartroutine/category/repository/CategoryRepository.java
+++ src/main/java/com/smartroutine/category/repository/CategoryRepository.java
@@ -1,6 +1,8 @@
 package com.smartroutine.category.repository;
 
+import com.smartroutine.category.dto.CategoryReadResponse;
 import com.smartroutine.category.entity.Category;
+import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,4 +11,6 @@ import org.springframework.stereotype.Repository;
 public interface CategoryRepository extends JpaRepository<Category, UUID> {
 
     boolean existsByUserIdAndCategoryName(UUID userId, String categoryName);
+
+    List<Category> findAllByUserId(UUID userId);
 }
