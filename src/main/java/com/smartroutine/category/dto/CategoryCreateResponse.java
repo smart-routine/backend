c765fe6d89260b4f48acd2ad7904b0fc954b6a21
sohyun119 <sh-119@naver.com>
2026-03-13 00:09:10 +0900

    ✨feat: 카테고리 생성 ResponseDto record로 변환

--- src/main/java/com/smartroutine/category/dto/CategoryCreateResponse.java
+++ src/main/java/com/smartroutine/category/dto/CategoryCreateResponse.java
@@ -2,19 +2,7 @@ package com.smartroutine.category.dto;
 
 import com.smartroutine.category.entity.CategoryColor;
 import java.util.UUID;
-import lombok.Getter;
 
-@Getter
-public class CategoryCreateResponse {
-
-    private UUID id;
-    private String categoryName;
-    private CategoryColor color;
-
-    public CategoryCreateResponse(UUID id, String categoryName, CategoryColor color) {
-        this.id = id;
-        this.categoryName = categoryName;
-        this.color = color;
-    }
+public record CategoryCreateResponse(UUID id, String categoryName, CategoryColor color) {
 
 }
