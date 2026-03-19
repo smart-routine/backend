37c430b136c722d0c880d755e7a0eadd4b9303f0
sohyun119 <sh-119@naver.com>
2026-03-19 09:06:56 +0900

    🩹 fix: Category 코드 수정 - Exception 추가 - valid 추가

--- src/main/java/com/smartroutine/category/dto/CategoryUpdateRequest.java
+++ src/main/java/com/smartroutine/category/dto/CategoryUpdateRequest.java
@@ -1,7 +1,6 @@
 package com.smartroutine.category.dto;
 
 import com.smartroutine.category.entity.CategoryColor;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
