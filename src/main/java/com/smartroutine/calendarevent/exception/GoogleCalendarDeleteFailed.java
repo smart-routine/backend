d8a3ac0a4dc341ff1cc8242842818c1bf2773e0a
sohyun119 <sh-119@naver.com>
2026-07-20 17:30:43 +0900

    ✨ feat: 구글 캘린더 수정 및 삭제 기능 추가 - updateGoogleCalendar 메서드 - deleteGoogleCalendar 메서드

--- src/main/java/com/smartroutine/calendarevent/exception/GoogleCalendarDeleteFailed.java
+++
@@ -1,8 +0,0 @@
-package com.smartroutine.calendarevent.exception;
-
-public class GoogleCalendarDeleteFailed extends IllegalStateException {
-
-    public GoogleCalendarDeleteFailed() {
-        super("Google Calender event deleted failed");
-    }
-}
